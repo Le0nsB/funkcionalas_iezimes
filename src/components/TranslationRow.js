112dd36fdfe0236ae704b8347c3287ef768d3f55
Le0nsb <ipb23.l.bluks@vtdt.edu.lv>
2026-01-19 15:12:46 +0200

    funk iezimesssss

--- src/components/TranslationRow.js
+++ src/components/TranslationRow.js
@@ -1,8 +1,12 @@
 function TranslationRow({ en, lv }) {
+  const isKeyword = lv.startsWith("*");
+
   return (
     <tr>
       <td>{en}</td>
-      <td>{lv}</td>
+      <td style={{ color: isKeyword ? "#0066cc" : "inherit" }}>
+        {lv}
+      </td>
     </tr>
   );
 }
