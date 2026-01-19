5ea4fe6abb750422860fe312bf719997b8394785
Le0nsb <ipb23.l.bluks@vtdt.edu.lv>
2026-01-19 15:10:02 +0200

    funk iezimes

--- src/components/Translations.js
+++ src/components/Translations.js
@@ -1,12 +1,19 @@
 import TranslationTable from "./TranslationTable";
 
 const translations = [
-  { en: "Feature", lv: "Funkcionalitāte" },
-  { en: "Scenario", lv: "Scenārijs" },
-  { en: "Given", lv: "Kad" },
-  { en: "When", lv: "Ja" },
-  { en: "Then", lv: "Tad" },
+  { en: "Feature", lv: "Funkcionalitāte (Fīča)" },
+  { en: "Background", lv: "Konteksts / Situācija" },
+  { en: "Rule", lv: "Noteikums" },
+  { en: "Scenario", lv: "Scenārijs (Piemērs)" },
+  { en: "Scenario Outline", lv: "Scenārijs pēc parauga" },
+  { en: "Examples", lv: "Piemēri / Paraugs" },
+  { en: "Given", lv: "*Kad" },
+  { en: "When", lv: "*Ja" },
+  { en: "Then", lv: "*Tad" },
+  { en: "And", lv: "*Un" },
+  { en: "But", lv: "*Bet" }
 ];
+
 
 function Translations() {
   return <TranslationTable data={translations} />;
