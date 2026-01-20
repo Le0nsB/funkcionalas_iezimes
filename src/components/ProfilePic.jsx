9c492dfbb212ce7905565c3ec8329e91768ad03c
Le0nsb <ipb23.l.bluks@vtdt.edu.lv>
2026-01-20 13:54:53 +0200

    m

--- src/components/ProfilePic.jsx
+++ src/components/ProfilePic.jsx
@@ -9,10 +9,13 @@ function ProfilePic() {
         <li>Es vēlos pievienot profila attēlu</li>
         <li>Lai mani varētu vieglāk atpazīt</li>
       </ul>
-        <h3 color="#b93455">Scenārijs</h3>
+        <h3 color="#b93455">LabvēlīgsScenārijs</h3>
         Kad lietotājs grib augšupielādēt profila bildi (Given), Ja viņš izvēlas
         attēlu no sava datora (When), Tad attēls tiek pievienots viņa
         profilam (Then).
+         <h3 color="#b93455">Nelabvēlīgs Scenārijs</h3>
+        Kad lietotājs grib augšupielādēt profila bildi (Given), Ja viņš izvēlas
+        attēlu no sava datora (When), Tad attēls tiek nepieņemts dēļ faila izmēra (Then).
       </p>
 
     </>
